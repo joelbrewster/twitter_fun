efca7eba859482eb7e6cf1122942347292b9755a
Joel <j.n.brewster@gmail.com>
2016-05-27 09:46:06 +1000

    Add all the things

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :posts
+
+  get '/' => 'posts#home'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+end
