7edd620f1884df13c58820e72e19889fd7f7cf9e
Joel <j.n.brewster@gmail.com>
2016-05-27 10:42:27 +1000

    js for hiding and showing quotes

---
+++ app/assets/javascripts/togglequotes.js
@@ -0,0 +1,17 @@
+$(function() {
+// # Grabbing teh section corresponding tot he button on button click, show the section
+  $('#btn-quote').on('click', function() )
+    $('.stuff').hide();
+    $('#quote').show();
+  });
+
+  $('#btn-chuck').on('click', function() )
+    $('.stuff').hide();
+    $('#chuck').show();
+  });
+
+  $('#btn-cat').on('click', function() )
+    $('.stuff').hide();
+    $('#catfact').show();
+  });
+})
